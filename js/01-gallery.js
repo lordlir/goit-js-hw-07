606dbe638d930c9859c62642dc360be189b8d9ad
Bohdan <666lordlir999@gmail.com>
2022-12-05 00:06:29 +0100

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryOfImg = document.querySelector(".gallery");
+
+galleryOfImg.insertAdjacentHTML("afterbegin", createListOfImg(galleryItems));
+
+function createListOfImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+galleryOfImg.addEventListener("click", openOriginImg);
+
+function openOriginImg(event) {
+  event.preventDefault();
+
+  //   console.log(event.target);
+  //     console.log(event.currentTarget);
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const originImgSie = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${originImgSie}" >
+`);
+
+  instance.show();
+
+  window.addEventListener("keydown", closeOfEscape);
+  function closeOfEscape(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryOfImg = document.querySelector(".gallery");
+
+galleryOfImg.insertAdjacentHTML("afterbegin", createListOfImg(galleryItems));
+
+function createListOfImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
